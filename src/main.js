10465ac16568ff95b4b1c36f8d4112c7e14d554d
Emil Flatz <emil.flatz@gmail.com>
2025-03-30 19:52:37 +0200

    init

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import './style.css'
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.js'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+    </a>
+    <h1>Hello D3!</h1>
+    <p>
+      <a href="/pages/01_drawing_divs.html">Go to Drawing Divs Page</a>
+    </p>
+  </div>
+`
+
